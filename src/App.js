5a5fc2d5aea1121d602f00f29d56b257cdf166f0
SelyanKab <selyan.kab@protonmail.com>
2022-01-16 12:30:37 +0100

    tazwara

--- src/App.js
+++ src/App.js
@@ -7,15 +7,15 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Asenfar amenzu s Recat.js
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://imsidag.com"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Imsidag Iqbayliyen
         </a>
       </header>
     </div>
